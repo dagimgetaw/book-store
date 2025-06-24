223422995aac5714827e663c2f4cf95f31486a9a
Dagim <dagimgetaW27@gmail.com>
2025-06-24 09:54:36 +0300

    fix: homepage route method

--- client/src/Components/Notfound/Notfound.jsx
+++ client/src/Components/Notfound/Notfound.jsx
@@ -1,10 +1,9 @@
 import useStore from "../../store/useStore.js";
 import { ArrowLeft } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   const { theme } = useStore();
-  const navigate = useNavigate();
 
   return (
     <div
@@ -40,17 +39,18 @@ export default function NotFound() {
           The page you're looking for doesn't exist or has been moved.
         </p>
 
-        <button
-          onClick={() => navigate("/")}
-          className={`flex items-center justify-center gap-2 px-6 py-3 cursor-pointer rounded-full font-medium transition-all duration-300 hover:shadow-lg focus:outline-none focus:ring-2 ${
-            theme === "light"
-              ? "bg-indigo-600 hover:bg-indigo-700 text-white focus:ring-indigo-400 shadow-indigo-500/30"
-              : "bg-indigo-500 hover:bg-indigo-600 text-white focus:ring-indigo-300 shadow-indigo-500/20"
-          } shadow-md hover:-translate-y-0.5 mx-auto`}
-        >
-          <ArrowLeft className="w-5 h-5" />
-          Return to Homepage
-        </button>
+        <Link to="/">
+          <button
+            className={`flex items-center justify-center gap-2 px-6 py-3 cursor-pointer rounded-full font-medium transition-all duration-300 hover:shadow-lg focus:outline-none focus:ring-2 ${
+              theme === "light"
+                ? "bg-indigo-600 hover:bg-indigo-700 text-white focus:ring-indigo-400 shadow-indigo-500/30"
+                : "bg-indigo-500 hover:bg-indigo-600 text-white focus:ring-indigo-300 shadow-indigo-500/20"
+            } shadow-md hover:-translate-y-0.5 mx-auto`}
+          >
+            <ArrowLeft className="w-5 h-5" />
+            Return to Homepage
+          </button>
+        </Link>
       </div>
     </div>
   );
