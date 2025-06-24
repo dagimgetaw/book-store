3026236480c4b49611562997ec98c477617572d8
Dagim <dagimgetaW27@gmail.com>
2025-06-24 09:53:14 +0300

    feat: link btns to routes

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Album, Search, Sun, Moon } from "lucide-react";
 import useStore from "../../store/useStore.js";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const { theme, toggleTheme } = useStore();
@@ -13,16 +14,18 @@ export default function Navbar() {
       }`}
     >
       <div className="mx-auto w-full flex justify-between items-center py-3 md:py-6 px-6 md:px-12 lg:px-16">
-        <div className="flex items-center gap-2 cursor-pointer group">
-          <Album
-            className={`w-7 h-7 transition-transform duration-300 group-hover:rotate-12 ${
-              theme === "light" ? "text-indigo-400" : "text-indigo-600"
-            }`}
-          />
-          <p className="text-lg md:text-2xl font-bold bg-gradient-to-r from-indigo-500 to-purple-600 bg-clip-text text-transparent">
-            BookStore
-          </p>
-        </div>
+        <Link to="/">
+          <div className="flex items-center gap-2 cursor-pointer group">
+            <Album
+              className={`w-7 h-7 transition-transform duration-300 group-hover:rotate-12 ${
+                theme === "light" ? "text-indigo-400" : "text-indigo-600"
+              }`}
+            />
+            <p className="text-lg md:text-2xl font-bold bg-gradient-to-r from-indigo-500 to-purple-600 bg-clip-text text-transparent">
+              BookStore
+            </p>
+          </div>
+        </Link>
 
         <form
           className={`flex items-center rounded-full px-3 py-1 md:px-4 md:py-2 focus-within:ring-2 transition-all w-32 md:w-64 lg:w-80 shadow-sm ${
@@ -63,16 +66,17 @@ export default function Navbar() {
               <Moon className="w-6 h-6" strokeWidth={1.5} />
             )}
           </button>
-
-          <button
-            className={`px-5 py-2 rounded-full font-medium transition-all duration-300 hover:shadow-lg focus:outline-none focus:ring-2 cursor-pointer ${
-              theme === "light"
-                ? "bg-indigo-600 hover:bg-indigo-700 text-white focus:ring-indigo-400 shadow-indigo-500/30"
-                : "bg-indigo-500 hover:bg-indigo-600 text-white focus:ring-indigo-300 shadow-indigo-500/20"
-            } shadow-md hover:-translate-y-0.5`}
-          >
-            Sign Up
-          </button>
+          <Link to="/login">
+            <button
+              className={`px-5 py-2 rounded-full font-medium transition-all duration-300 hover:shadow-lg focus:outline-none focus:ring-2 cursor-pointer ${
+                theme === "light"
+                  ? "bg-indigo-600 hover:bg-indigo-700 text-white focus:ring-indigo-400 shadow-indigo-500/30"
+                  : "bg-indigo-500 hover:bg-indigo-600 text-white focus:ring-indigo-300 shadow-indigo-500/20"
+              } shadow-md hover:-translate-y-0.5`}
+            >
+              Login
+            </button>
+          </Link>
         </div>
       </div>
     </nav>
