423f4fa4dfaf9e552e787b931a44c88fd8253009
Dagim <dagimgetaW27@gmail.com>
2025-06-26 23:27:47 +0300

    style: add md screen level loading w & h

--- client/src/Components/Loading/Loading.jsx
+++ client/src/Components/Loading/Loading.jsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <svg
-      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+      className="animate-spin -ml-1 mr-3 w-4 h-4 md:w-5 md:h-5 text-white"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
